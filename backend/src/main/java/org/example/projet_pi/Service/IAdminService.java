99ad1c34f76acefa1154fd09e15cce9d9cd0e5c6
Tasnim847 <tasnim.ghodbane@esprit.tn>
2026-04-09 01:12:25 +0100

    modif

--- backend/src/main/java/org/example/projet_pi/Service/IAdminService.java
+++ backend/src/main/java/org/example/projet_pi/Service/IAdminService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.example.projet_pi.Service;
 
 import org.example.projet_pi.entity.Admin;
@@ -17,24 +16,4 @@ public interface IAdminService {
     Admin getAdminById(Long id);
 
     List<Admin> getAllAdmins();
-=======
-package org.example.projet_pi.Service;
-
-import org.example.projet_pi.entity.Admin;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-public interface IAdminService {
-
-    Admin addAdmin(Admin admin, MultipartFile photo);
-
-    Admin updateAdminById(Long id, Admin admin, MultipartFile photo);
-
-    void deleteAdmin(Long id);
-
-    Admin getAdminById(Long id);
-
-    List<Admin> getAllAdmins();
->>>>>>> f0c4e72 (url de front)
 }
