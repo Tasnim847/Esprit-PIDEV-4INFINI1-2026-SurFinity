01e7e4337fb7e7666e511cc57bce448b7bc61603
nesrineee88 <nesrine.nabli@esprit.tn>
2026-04-10 10:11:12 +0100

    login

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http'; // ← Ajoutez cette ligne
+import { provideHttpClient, withFetch, withInterceptors ,HttpInterceptorFn } from '@angular/common/http';
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+  return next(req);
+};
 
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -13,26 +13,26 @@ export class SidebarComponent {
   @Output() sidebarToggle = new EventEmitter<boolean>();
 
   menuItems = [
-    { label: 'Home', icon: '🏠', link: '/dashboard' },
-    { label: 'Credit', icon: '💳', link: '/dashboard/credit' },
-    { label: 'Insurance', icon: '🛡️', link: '/dashboard/insurance' },
-    { label: 'Account', icon: '👤', link: '/dashboard/account' },
-    { label: 'Complaint', icon: '📄', link: '/dashboard/complaint' },
-    { label: 'News', icon: '📰', link: '/dashboard/news' },
-    { label: 'Products', icon: '📦', link: '/dashboard/products' }
+    { label: 'Home', icon: '🏠', link: '/backoffice' },
+    { label: 'Credit', icon: '💳', link: '/backoffice/credit' },
+    { label: 'Insurance', icon: '🛡️', link: '/backoffice/insurance' },
+    { label: 'Account', icon: '👤', link: '/backoffice/account' },
+    { label: 'Complaint', icon: '📄', link: '/backoffice/complaint' },
+    { label: 'News', icon: '📰', link: '/backoffice/news' },
+    { label: 'Products', icon: '📦', link: '/backoffice/products' }
   ];
 
   isCollapsed = false;
 
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
-    
+
     if (this.isCollapsed) {
       document.body.classList.add('sidebar-collapsed');
     } else {
       document.body.classList.remove('sidebar-collapsed');
     }
-    
+
     // Émettre l'événement
     this.sidebarToggle.emit(this.isCollapsed);
   }
